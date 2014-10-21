f936571e1d93d40c5710ca928dcbdd01f63022c3
OzTamir <TheOzTamir@gmail.com>
2014-10-21 21:36:58 +0300

    Actual Pebble Code

---
+++ Pebble App/src/app.js
@@ -0,0 +1,73 @@
+/**
+ * Pebble Mouse v0.1
+ * Written by Oz Tamir
+ * Website: http://www.oztamir.com
+ * The Code for the server can be found here:
+ * http://www.github.com/OzTamir/PebbleMouse
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Accel = require('ui/accel');
+var isRunning = false;
+var main = new UI.Card({
+  title: 'Pebble Mouse',
+  subtitle: 'Server: Not connected',
+  body: 'Looking for the server...',
+  style: 'mono'
+});
+
+main.show();
+Accel.init();
+Accel.config({rate : 100});
+
+var connection = new WebSocket('ws://192.168.1.12:8000');
+
+connection.onopen = function () {
+  connection.send('0');
+  main.subtitle("Server: Waiting for data")
+  main.body('Press Select to start sending data to the server.');
+};
+
+Accel.on('data', function(e) {
+  if (isRunning) {
+    connection.send(JSON.stringify(e.accels));
+  }
+});
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+  main.title("Error");
+  main.body("Couldn't connect to the server, please make sure it's running.");
+};
+
+connection.onclose = function (e) {
+  main.title("Error");
+  main.subtitle("Server: Closed")
+  main.body("The Server disconnected. Please reset both this app and the server to continue.");
+}
+
+main.on('click', 'select', function(e) {
+  if (isRunning || connection.readyState == 3) {
+    isRunning = false;
+    main.body('Currently not running. Press Select to start.');
+  }
+  else {
+    isRunning = true;
+    main.subtitle("Server: Running")
+    main.body("Stop: Press Select\nLeft Click: Press Up\nRight Click: Press Down");
+  }
+});
+
+main.on('click', 'up', function(e) {
+  if (isRunning || connection.readyState == 3) {
+   connection.send("1"); 
+  }
+});
+
+main.on('click', 'down', function(e) {
+  if (isRunning || connection.readyState == 3) {
+   connection.send("2"); 
+  }
+});
