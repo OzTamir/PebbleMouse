e0e1fa3e886028f8b76bf1074638d1147e1a38e1
OzTamir <TheOzTamir@gmail.com>
2014-10-22 12:30:35 +0300

    Added IP Selection Screen

--- Pebble App/src/app.js
+++ Pebble App/src/app.js
@@ -6,10 +6,14 @@
  * http://www.github.com/OzTamir/PebbleMouse
  * This is where you write your app.
  */
-
 var UI = require('ui');
+var Vector2 = require('vector2');
 var Accel = require('ui/accel');
+
+// Used to run this code from app.js
+var mainFunc = function(ip) {
 var isRunning = false;
+
 var main = new UI.Card({
   title: 'Pebble Mouse',
   subtitle: 'Server: Not connected',
@@ -21,11 +25,11 @@ main.show();
 Accel.init();
 Accel.config({rate : 100});
 
-var connection = new WebSocket('ws://192.168.1.12:8000');
+var connection = new WebSocket('ws://' + ip + ':8000');
 
 connection.onopen = function () {
   connection.send('0');
-  main.subtitle("Server: Waiting for data")
+  main.subtitle("Server: Waiting for data");
   main.body('Press Select to start sending data to the server.');
 };
 
@@ -44,9 +48,9 @@ connection.onerror = function (error) {
 
 connection.onclose = function (e) {
   main.title("Error");
-  main.subtitle("Server: Closed")
+  main.subtitle("Server: Closed");
   main.body("The Server disconnected. Please reset both this app and the server to continue.");
-}
+};
 
 main.on('click', 'select', function(e) {
   if (isRunning || connection.readyState == 3) {
@@ -55,7 +59,7 @@ main.on('click', 'select', function(e) {
   }
   else {
     isRunning = true;
-    main.subtitle("Server: Running")
+    main.subtitle("Server: Running");
     main.body("Stop: Press Select\nLeft Click: Press Up\nRight Click: Press Down");
   }
 });
@@ -69,5 +73,55 @@ main.on('click', 'up', function(e) {
 main.on('click', 'down', function(e) {
   if (isRunning || connection.readyState == 3) {
    connection.send("2"); 
+  }
+});
+  
+};
+
+// -----------------------------------
+
+
+// Load a basic screen
+var wind = new UI.Window(); 
+var textfield = new UI.Text({
+  position: new Vector2(0, 50),
+  size: new Vector2(144, 80),
+  font: 'gothic-28-bold',
+  text: "Set Server IP",
+  textAlign: 'center'
+});
+wind.add(textfield);
+var ip = ["192", "168", "0", "0"];
+var current = 0;
+
+var count = new UI.Text({
+  position: new Vector2(0, 100),
+  size: new Vector2(144, 60),
+  font: 'gothic-24-bold',
+  text: "192",
+  textAlign: 'center'
+});
+wind.add(count);
+wind.show();
+
+wind.on('click', 'up', function() {
+  ip[current] = (parseInt(ip[current]) + 1).toString();
+  count.text(ip[current]);
+});
+
+wind.on('click', 'down', function() {
+  ip[current] = (parseInt(ip[current]) - 1).toString();
+  count.text(ip[current]);
+});
+ 
+wind.on('click', 'select', function() {
+  if (current < 3) {
+    current += 1;
+    count.text(ip[current]);
+  }
+  else {
+    var ipString = ip.join(".");
+    wind.hide();
+    mainFunc(ipString);
   }
 });
